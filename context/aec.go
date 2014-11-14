c907ad60e6976b099aa0319399d4f6e7f2b00671
Tim Greiser <tgreiser@gmail.com>
2014-11-14 14:08:25 -0800

    context - removed unneeded namespace code from Aec

--- context/aec.go
+++ context/aec.go
@@ -7,18 +7,6 @@ import(
 
 func Aec(req *http.Request) appengine.Context {
   aec := appengine.NewContext(req)
-  ns := req.Header.Get("X-Namespace")
-  if ns == "" {
-    ns = "prod"
-  }
-
-  var err error
-  aec.Infof("Setting namespace: %v", ns)
-  aec, err = appengine.Namespace(aec, ns)
-  if err != nil {
-    aec = appengine.NewContext(req)
-    aec.Errorf("Error loading namespace!")
-  }
 
   return aec
 }
