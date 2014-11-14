94a54340be690ee4574c802e262def1a55b017f0
Tim Greiser <tgreiser@gmail.com>
2014-11-14 14:09:26 -0800

    view/draft - new view for previewing a draft content_controller - partials system implemented

--- controllers/content_controller.go
+++ controllers/content_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
   "bytes"
   "html/template"
+  "path/filepath"
 
   "github.com/russross/blackfriday"
   "github.com/stretchr/goweb"
@@ -17,7 +18,9 @@ type ContentController struct {}
 Show the markdown editor form
 */
 func (ctrl *ContentController) New(c context.Context) error {
-  t := template.Must(template.ParseFiles("views/new.html"))
+  wc := mycontext.NewContext(c)
+  matches, _ := ctrl.templates(wc, "new.html")
+  t := template.Must(template.ParseFiles(matches...))
   var output bytes.Buffer
   data := struct {
     Title string
@@ -25,7 +28,21 @@ func (ctrl *ContentController) New(c context.Context) error {
     "Test Page",
   }
   t.Execute(&output, data )
+  wc.Aec.Infof("Template: %v", t.Tree)
   return goweb.Respond.With(c, 200, output.Bytes())
+}
+
+func (ctrl *ContentController) templates(wc mycontext.Context, main string) ([]string, error) {
+  pattern := filepath.Join("views", "partials", "*.html")
+  wc.Aec.Infof("Pattern: %v", pattern)
+  matches, err := filepath.Glob(pattern)
+  if err != nil {
+    wc.Aec.Errorf("Error finding files: %v", err)
+  } else {
+    matches = append([]string{filepath.Join("views", main)}, matches...)
+    wc.Aec.Infof("Found matches: %v", matches)
+  }
+  return matches, err
 }
 
 func (ctrl *ContentController) Create(c context.Context) error {
@@ -33,7 +50,7 @@ func (ctrl *ContentController) Create(c context.Context) error {
 
   wc.Aec.Infof("Running create %v", c.FormValue("content"))
 
-  t := template.Must(template.ParseFiles("templates/simple/index.html"))
+  draft := template.Must(template.ParseFiles("templates/simple/index.html"))
   var output bytes.Buffer
   data := struct {
     Content template.HTML
@@ -42,7 +59,18 @@ func (ctrl *ContentController) Create(c context.Context) error {
     template.HTML(blackfriday.MarkdownBasic([]byte(c.FormValue("content")))),
     c.FormValue("title"),
   }
-  t.Execute(&output, data )
-  return goweb.Respond.With(c, 200, output.Bytes())
+  draft.Execute(&output, data )
+
+  matches, _ := ctrl.templates(wc, "draft.html")
+  t := template.Must(template.ParseFiles(matches...))
+  var page bytes.Buffer
+  draftdata := struct {
+    Draft template.HTML
+  }{
+    template.HTML(output.String()),
+  }
+  t.Execute(&page, draftdata )
+
+  return goweb.Respond.With(c, 200, page.Bytes())
 
 }
